c4aa85a6b0637a025b37fc9bfa80fc8f4476ea9f
Vladimir Gaus <venic89@gmail.com>
2024-06-06 17:43:59 +0200

    Add files via upload

--- run.py
+++ run.py
@@ -2,17 +2,16 @@ from sysauditpro.auditor import perform_audit
 from sysauditpro.reporter import generate_report, save_report
 
 def main():
-    # Create security audit
+    # Führe das Sicherheitsaudit durch
     audit_results = perform_audit()
 
-    # Generate pdf report from events
+    # Generiere einen PDF-Bericht basierend auf den Audit-Ergebnissen
     report_file = generate_report(audit_results)
 
-    # save the file
+    # Optional: Speichere oder sende den Bericht
     save_report(report_file)
 
-    print(f"The report successfully save as: {report_file}")
+    print(f"Der Bericht wurde erfolgreich erstellt und gespeichert als: {report_file}")
 
 if __name__ == '__main__':
     main()
- 
